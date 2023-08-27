49cdffbe989bacad9094f6d66cd4ec886c2eba64
anahi <anahibenabides@gmail.com>
2023-08-27 17:11:31 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let nombre = "anahi";
+let apellido = "benabides";
+
+
+console.log(nombre + " " + apellido);
+
+const num1 = "30"
+const num2 = "3"
+
+console.log(30+3);
+
+const num3 = 1
+
+console.log(33+1);
